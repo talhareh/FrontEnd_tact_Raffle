a6b802c48f1539c1301a2e6b7774446a993f1c15
talhareh <talha59@gmail.com>
2024-08-14 12:31:30 +0400

    typescript issue #2

--- src/hooks/useRuffContract.tsx
+++ src/hooks/useRuffContract.tsx
@@ -2,17 +2,23 @@ import { useAsyncInitialize } from "./useAsyncInitialize";
 import { useTonClient } from "./useTonClient";
 import { Ruffruff } from "../wrappers/Ruffruff";
 import { Address, OpenedContract } from "@ton/core";
+import { useState } from "react";
 
 export function useRuffContract(){
     const {client} = useTonClient()
 
-    const ruffCon = useAsyncInitialize( async()=>{
+    const [list, setList] = useState(null)
+
+    const ruffCon: OpenedContract<Ruffruff> | undefined = useAsyncInitialize( async()=>{
         if(!client) return;
 
-        const contract = Ruffruff.fromAddress(Address.parse("EQB7vFlIfwZvW2uJWkBZmYTMNE5XiAPmGVE-Sq5MrWKlYWpW"))
+        const contract =  Ruffruff.fromAddress(Address.parse("EQB7vFlIfwZvW2uJWkBZmYTMNE5XiAPmGVE-Sq5MrWKlYWpW"))
 
-        return client.open(ruffCon) as OpenedContract<Ruffruff>
+        return client.open(contract) as OpenedContract<Ruffruff>
     }, [client])
+
+    
+    return {ruffCon}
 
     
 }
